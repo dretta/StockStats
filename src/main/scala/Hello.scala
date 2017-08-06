b4f6912f22a90aee81153be0ce6e48c7d5599adc
dretta <dretta@ymail.com>
2017-08-06 17:58:57 -0600

    Removed Hello files

--- src/main/scala/Hello.scala
+++
@@ -1,7 +0,0 @@
-object Hello extends Greeting with App {
-  println(greeting)
-}
-
-trait Greeting {
-  lazy val greeting: String = "hello"
-}
--- src/test/scala/HelloSpec.scala
+++
@@ -1,7 +0,0 @@
-import org.scalatest._
-
-class HelloSpec extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
-  }
-}
